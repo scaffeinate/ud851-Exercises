2f7e56886731cba99be040cae48ed35741c8c17a
Sudharsanan <sudhar.ti@gmail.com>
2017-10-05 15:58:41 -0400

    Completed exercise T02.03

--- Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
+++ Lesson02-GitHub-Repo-Search/T02.03-Exercise-DisplayUrl/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
@@ -15,9 +15,13 @@
  */
 package com.example.android.datafrominternet.utilities;
 
+import android.net.Uri;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -44,9 +48,12 @@ public class NetworkUtils {
      * @param githubSearchQuery The keyword that will be queried for.
      * @return The URL to use to query the weather server.
      */
-    public static URL buildUrl(String githubSearchQuery) {
-        // TODO (1) Fill in this method to build the proper Github query URL
-        return null;
+    public static URL buildUrl(String githubSearchQuery) throws MalformedURLException {
+        // COMPLETED (1) Fill in this method to build the proper Github query URL
+        return new URL(new Uri.Builder()
+                .path(GITHUB_BASE_URL)
+                .appendQueryParameter(PARAM_QUERY, githubSearchQuery)
+                .appendQueryParameter(PARAM_SORT, sortBy).build().toString());
     }
 
     /**
