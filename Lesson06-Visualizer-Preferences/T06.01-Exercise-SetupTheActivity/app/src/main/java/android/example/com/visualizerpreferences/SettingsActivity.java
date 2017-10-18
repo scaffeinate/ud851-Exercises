be9e53431820cc344fcb610fdf0ec082437c63b4
Sudharsanan <sudhar.ti@gmail.com>
2017-10-18 12:42:42 -0400

    Finish T06.01

---
+++ Lesson06-Visualizer-Preferences/T06.01-Exercise-SetupTheActivity/app/src/main/java/android/example/com/visualizerpreferences/SettingsActivity.java
@@ -0,0 +1,16 @@
+package android.example.com.visualizerpreferences;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by sudhar on 10/18/17.
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
